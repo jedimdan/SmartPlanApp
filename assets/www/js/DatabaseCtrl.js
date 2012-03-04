bdc83ddb851b6d9d96b2ffe10a180d39650a4957
Daniel Tsou <jedimdan@gmail.com>
2012-03-05 00:57:17 +0800

    Updated Database CTRL to provide more details on what to code.

--- assets/www/js/DatabaseCtrl.js
+++ assets/www/js/DatabaseCtrl.js
@@ -1,16 +1,16 @@
 //on launch
 //check if table exists, and create if not
-//create database
+//create database with two tables. Task: ID, duedate, duration, name. Workhours: id, day, starthr, endhr.
 
-//reset database
+//reset database (not important yet)
 
-//bootstrap database for testing
+//bootstrap database for testing (don't need to care first)
 
-//create task
+//create a task
 
 //retrieve tasks with 'done' column set as false. Returns an array of Task objects. Needs to be order by duedate
 
-//update task
+//update task (params: task object). Update the entire task based on this task object using the object's ID column.
 
 //delete task
 
